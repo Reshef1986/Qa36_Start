1926f09b3705b295f8ece2b9237371edc8151703
Reshef1986 <reshef1986@gmail.com>
2022-12-24 11:27:08 +0200

    Update Login.java
    СssHomWork

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -95,14 +95,67 @@ elLogin.click();
 
  }
 
-public  void  ccslocatrstest (){
+    @Test
+    public void ccslocatrstest() {
+
+        // нужно перейти на страницу логина, так как на главной странице мало элементов для поиска и тренировки
+        // переход на страницу логина тут:
+        wd.findElement(By.linkText("LOGIN")).click();
+
+        // теперь мы на странице логина и тут будем тренироваться
+
+
+        // На странице логин найти несколько элементов по их  tagname
+        wd.findElement(By.tagName("div"));
+        wd.findElement(By.cssSelector(" [name='login' ]"));
+        wd.findElement( By.cssSelector("[name = 'registration']"));
+
+        // ON Login PAGE find some elements by class
+wd.findElement(By.cssSelector(".active"));
+        // На странице логин найти несколько элементов по их  attribute
+        wd.findElement(By.cssSelector("[aria-current='page']"));
+wd.findElement(By.cssSelector("[placeholder ='Email']"));
+wd.findElement(By.cssSelector("[placeholder ='Password']"));
+/* Можно еще найти первую кнопку Login по wd.findElement(By.cssSelector("[type='submit']"));
+Так как вы говорили что идет сверху вниз а  таких элементов 2 и этот первый
+ */
+
+        //На странице логин найти  элемент с  атрибутом placeholder, значение которого начинается на "Pass"
+      wd.findElement(By.cssSelector("[placeholder ^='Pass']"));
+
+        //На странице логин найти  элемент с  атрибутом placeholder, значение которого  начинается на  "ass"
+        wd.findElement(By.cssSelector("[placeholder *='ass']"));
+        //На странице логин найти  элемент с  атрибутом placeholder, значение которого заканчивается на "ord"
+        wd.findElement(By.cssSelector("[placeholder $='ord']"));
+        // Объединение !!
+        //найти элемент div, у которого есть id со значением "root"  и класс со значением container
+        wd.findElement(By.cssSelector(" div#root.container "));
+
+
+        // найти элемент а, у которого есть атрибут href со значением /login
+        wd.findElement(By.cssSelector("a[href ='/login'"));
+        // найти элемент input, у которого есть атрибут name cо значением email
+        wd.findElement(By.cssSelector("input[name='email']"));
+        // найти элемент button, у которого есть атрибут name cо значением login
+        wd.findElement(By.cssSelector("button[name='login']"));
+
+        // Поиск по дочерним элементам (* задание со звездоской)
+        // Найти кнопку регистрации, как последнего ребенка элемента form
+
+wd.findElement(By.cssSelector("form *:last-child"));
+/*
+Form- форма
+* -Говорит что дай мне весь список и ужен внутри найти мне команда ниже
+last -child - последнего из списка
+nth-child() - можно указать какой именно например  nth-child(2)
+form *:first-child - первый
+ */
+
 
 }
-
- @AfterMethod
- public void postCondition(){
-
-  //wd.quit();
- }
-
+    @AfterMethod
+    public void postCondition(){
+        // close browser
+        // wd.quit();
+    }
 }
