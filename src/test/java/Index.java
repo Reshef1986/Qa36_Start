8f0f4dca8ac614f7740c721e3d3032d434050f83
Reshef1986 <reshef1986@gmail.com>
2022-12-30 20:49:01 +0200

    HomWork4

--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -35,14 +35,57 @@ public class Index {
         //правда ли что ячейка содержит строку Mexico
         WebElement lastCell = wd.findElement(By.cssSelector("tr:last-child td:last-child"));
         String str = lastCell.getText();
-        boolean res =str.contains("Mexico");
+        boolean res = str.contains("Mexico");
         Assert.assertTrue(res);
         Assert.assertTrue(str.contains("Mexico"));
 
+    }
+
+    @Test
+    public void itemsTests() {
+        // find Item1 & click() ===> assert that "div-alert" contains message  "Clicked by Item 1"
+        WebElement item11 = wd.findElement(By.cssSelector("[href = '#item1']"));
+        // wd.findElement(By.xpath("//*[@ href ='#item1']"));
+        item11.click();
+        WebElement squareRes = wd.findElement(By.cssSelector("#alert"));
+        wd.findElement(By.xpath("//*[@id ='alert']"));
+        System.out.println("Text in square after click Item 1 =  " + squareRes.getText());
+        Assert.assertTrue(squareRes.getText().contains("Clicked by Item 1"));
+
+        // find Item3 & click() ===> assert that "div-alert" contains message  "Clicked by Item 3"
+        WebElement item2 = wd.findElement(By.cssSelector("[href = '#item3']"));
+        //wd.findElement(By.xpath("//*[@href ='#item3' ]"));
+        item2.click();
+        System.out.println("Text in square after click Item 3 =  " + squareRes.getText());
+        Assert.assertTrue(squareRes.getText().contains("Clicked by Item 3"));
+    }
+
+    @Test
+    public void formTests()  {
+        // fill name & fill surename & click send
+        WebElement fildName = wd.findElement(By.cssSelector("[name = 'name']"));
+       // wd.findElement(By.xpath("//*[@name = 'name']"));
+        fildName.click();
+        fildName.clear();
+        fildName.sendKeys("Alex");
+        WebElement fildSureName = wd.findElement(By.cssSelector("[name ='surename']"));
+      //  wd.findElement(By.xpath("//*[@name='surename']"));
+        fildSureName.click();
+        fildSureName.clear();
+        fildSureName.sendKeys("Zaxarov");
+
+        WebElement sendButton = wd.findElement(By.cssSelector(".btn"));
+        //wd.findElement(By.xpath("//*[@class='btn']"));
+        sendButton.click();
+
+       WebElement squareRes = wd.findElement(By.cssSelector("#alert"));
+      // wd.findElement(By.xpath("//*[@id='alert']"));
+        System.out.println("You entered a first and last name = " + squareRes.getText());
+        // Assert that "div-alert" contains text with name + surename
+
+        Assert.assertTrue(squareRes.getText().contains("Alex Zaxarov"));
 
 
     }
-
- // правда ли что последния строка содержит Мехико
 
 }
